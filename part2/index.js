8b6d582bf940a9a7fa6a3d368026f8bca1d00299
dondifer <dondifer@gmail.com>
2023-04-26 10:18:44 +0200

    feat: part 2

---
+++ part2/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const url = require("url");
+
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+
+    const filename = "./pages" + q.pathname + ".html";
+
+    fs.readFile(filename, (err, data) => {
+      try {
+        res.writeHead(200, { "Content-Type": "text/html" });
+
+        res.write(data);
+
+        return res.end("");
+      } catch (error) {
+        fs.readFile("./pages/notFound.html", (err, data) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+
+          console.error(error);
+
+          return res.end(data);
+        });
+      }
+    });
+  })
+  .listen(8080);
